1e1d35f76471e5b52b0bbcb81ca22dd0cc9c455e
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-07-20 15:24:03 +0900

    Use: short variable declarations at another func

--- lesson.go
+++ lesson.go
@@ -7,12 +7,15 @@ var f64 float64 = 1.2
 var s string = "test"
 var t, f bool = true, false
 
-func main() {
-	fmt.Println(i, f64, s, t, f)
-
+func foo() {
 	xi := 1
 	xf64 := 1.2
 	xs := "test"
 	xt, xf := true, false
 	fmt.Println(xi, xf64, xs, xt, xf)
+}
+
+func main() {
+	fmt.Println(i, f64, s, t, f)
+	foo()
 }
