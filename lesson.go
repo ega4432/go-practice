85107a26804ff634b0d1d6de03b8f602e1a4c490
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-07-20 14:57:01 +0900

    Reset to base

--- lesson.go
+++ lesson.go
@@ -2,20 +2,6 @@ package main
 
 import "fmt"
 
-// comment
-
-/*
-this is comment for long area
-*/
-
-func init() {
-	fmt.Println("this it init!")
-}
-
-func bazz() {
-	fmt.Println("Bazz")
-}
 func main() {
-	bazz()
 	fmt.Println("Hello world!", "TEST TEST")
 }
