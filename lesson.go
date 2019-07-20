9f3354cbfa74a766405a275bd2c5049ba74517dc
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-07-20 18:25:17 +0900

    Deal with: numeric types

--- lesson.go
+++ lesson.go
@@ -2,31 +2,34 @@ package main
 
 import "fmt"
 
-const Pi = 3.14
-
-const (
-	Username = "test_user"
-	Password = "test_pass"
-)
-
-var (
-	i    int     = 1
-	f64  float64 = 1.2
-	s    string  = "test"
-	t, f bool    = true, false
-)
-
-func foo() {
-	xi := 1
-	xf64 := 1.2
-	xs := "test"
-	xt, xf := true, false
-	fmt.Println(xi, xf64, xs, xt, xf)
-	fmt.Printf("%T", xi)
-}
-
 func main() {
-	fmt.Println(i, f64, s, t, f)
-	foo()
-	fmt.Println(Pi, Username, Password)
+	var (
+		u8  uint8     = 255
+		i8  int8      = 127
+		f32 float32   = 0.2
+		c64 complex64 = -5 + 12i
+	)
+	fmt.Print(u8, i8, f32, c64)
+
+	fmt.Printf("type=%T value=%v", u8, u8)
+
+	x := 1 + 1
+	fmt.Println(x)
+	fmt.Println(1+1, 2+2)
+	fmt.Println("1 + 1 =", 1+1)
+	fmt.Println("10 - 1 =", 10-1)
+	fmt.Println("2 * 2", "2*2")
+	fmt.Println("10 / 2 = ", "10/2")
+	fmt.Println("10 / 3 =", 10/3)
+	fmt.Println("10.0 / 3 =", 10.0/3)
+	fmt.Println("10 / 3.0 =", 10.0/3.0)
+	fmt.Println("10 % 2 =", 10%2)
+	fmt.Println("10 % 3 =", 10%3)
+
+	y := 0
+	fmt.Println(y)
+	y++
+	fmt.Println(y)
+	y = y - 1
+	fmt.Println(y)
 }
