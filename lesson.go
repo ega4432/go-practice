ed590f3bca86a4cac3783f7fe60781652df8dfd3
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-07-20 20:09:30 +0900

    Deal with: aray and slice

--- lesson.go
+++ lesson.go
@@ -2,23 +2,33 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
-	var x int = 1
-	xx := float64(x)
-	fmt.Printf("%T %v %f\n", xx, xx, xx)
+	var a [2]int
+	a[0] = 100
+	a[1] = 200
+	fmt.Println(a)
 
-	var y float64 = 1.2
-	yy := int(y)
-	fmt.Printf("%T %v %d\n", yy, yy, yy)
+	var b []int = []int{100, 200}
+	b = append(b, 300)
+	fmt.Println(b)
 
-	var s string = "14"
-	i, _ := strconv.Atoi(s)
-	fmt.Printf("%T, %v\n", i, i)
+	n := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(n)
+	fmt.Println(n[2])
+	fmt.Println(n[2:4])
+	fmt.Println(n[2:])
+	fmt.Println(n[:2])
+	fmt.Println(n[:])
 
+	var board = [][]int{
+		[] int{0, 1, 2},
+		[] int{3, 4, 5},
+		[] int{6, 7, 8},
+	}
+	fmt.Println(board)
 
-	var h string = "Hello world"
-	fmt.Println(string(h[0]))
+	n = append(n, 100, 200, 300, 5239849)
+	fmt.Println(n)
 }
