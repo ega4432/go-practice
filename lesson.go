c66d86f1725c6de42fa719e017d7500f056039e1
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-07-21 16:25:34 +0900

    Use: function

--- lesson.go
+++ lesson.go
@@ -2,12 +2,35 @@ package main
 
 import "fmt"
 
-func main() {
-	b := []byte{72, 73}
-	fmt.Println(b)
-	fmt.Println(string(b))
+func add(x, y int) (int, int) {
+	return x + y, x - y
+}
 
-	c := []byte("HI")
-	fmt.Println(c)
-	fmt.Println(string(c))
+func cal(price, item int) (result int) {
+	result = price * item
+	return
+}
+
+func convert(price int) float64 {
+	return float64(price)
+}
+
+func main() {
+	r1, r2 := add(20, 10)
+	fmt.Println(r1, r2)
+
+	r3 := cal(1000, 3)
+	fmt.Println(r3)
+
+	r4 := convert(234)
+	fmt.Println(r4)
+
+	f := func(x int) {
+		fmt.Println("inner func", x)
+	}
+	f(1)
+
+	func(x int) {
+		fmt.Println("inner func", x)
+	}(2)
 }
