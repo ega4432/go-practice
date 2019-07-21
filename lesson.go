27e58cfb015924640c92560dd89ed3539ddfd1f8
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-07-21 17:17:43 +0900

    Excise: session3

--- lesson.go
+++ lesson.go
@@ -2,19 +2,23 @@ package main
 
 import "fmt"
 
-func foo(params ...int) {
-	fmt.Println(len(params), params)
-	for _, param := range params {
-		fmt.Println(param)
-	}
-}
-
 func main() {
-	foo(10, 30)
-	foo(10, 20, 30)
+	// Q1. 以下の 1.11 を int 型に変換して出力してください
+	f := 1.11
+	i := int(f)
+	fmt.Printf("%T %d\n", i, i)
+	fmt.Println(int(f))
 
-	s := []int{1, 2, 3}
-	fmt.Println(s)
+	// Q2. コードを書かずに以下の出力結果を答えてください。
+	s := []int{1, 2, 3, 5, 6, 2, 3, 1}
+	fmt.Println(s[2:4])
 
-	foo(s...)
+	// Q3. 以下のコードを実行した時に  "fmt.Printf("%T %v", m, m)"  以下のような出力結果となるmを作成してください。
+	// map[string]int map[Mike:20 Nancy:24 Messi:30]
+	m := map[string]int{
+		"Mike":  20,
+		"Nancy": 24,
+		"Messi": 30,
+	}
+	fmt.Printf("%T %v\n", m, m)
 }
