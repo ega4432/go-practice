e27a125f02f61bf589fa3c652458caa291e8da6d
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-07-21 18:18:42 +0900

    Use: if statement

--- lesson.go
+++ lesson.go
@@ -2,23 +2,38 @@ package main
 
 import "fmt"
 
-func main() {
-	// Q1. 以下の 1.11 を int 型に変換して出力してください
-	f := 1.11
-	i := int(f)
-	fmt.Printf("%T %d\n", i, i)
-	fmt.Println(int(f))
-
-	// Q2. コードを書かずに以下の出力結果を答えてください。
-	s := []int{1, 2, 3, 5, 6, 2, 3, 1}
-	fmt.Println(s[2:4])
-
-	// Q3. 以下のコードを実行した時に  "fmt.Printf("%T %v", m, m)"  以下のような出力結果となるmを作成してください。
-	// map[string]int map[Mike:20 Nancy:24 Messi:30]
-	m := map[string]int{
-		"Mike":  20,
-		"Nancy": 24,
-		"Messi": 30,
+func by2(num int) string {
+	if num%2 == 0 {
+		return "Odd"
+	} else {
+		return "Even"
 	}
-	fmt.Printf("%T %v\n", m, m)
+}
+func main() {
+	num := 7
+	if num%2 == 0 {
+		fmt.Println("by 2")
+	} else if num%3 == 0 {
+		fmt.Println("by 3")
+	} else {
+		fmt.Println("else!")
+	}
+
+	x, y := 10, 10
+	if x == 10 && y == 10 {
+		fmt.Println("$$")
+	} else if x == 10 || y == 10 {
+		fmt.Println("||")
+	} else {
+		fmt.Println("false")
+	}
+
+	result := by2(5)
+	if result == "Odd" {
+		fmt.Println("This num is odd!")
+	} else if result == "Even" {
+		fmt.Println("This num is Even!")
+	} else {
+		fmt.Println("ERROR!")
+	}
 }
