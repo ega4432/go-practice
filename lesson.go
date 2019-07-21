7da105bbb9997db84d02a0f863c08226159f1d9c
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-07-21 19:07:35 +0900

    Create: new branch for section4

--- lesson.go
+++ lesson.go
@@ -1,31 +1,4 @@
 package main
 
-import "fmt"
-
 func main() {
-	for i := 0; i < 10; i++ {
-		if i == 3 {
-			fmt.Println("continue")
-			continue
-		}
-
-		if i > 5 {
-			fmt.Println("beak")
-			break
-		}
-		fmt.Println(i)
-	}
-
-	sum := 1
-	fmt.Println(sum)
-	for sum < 10 {
-		sum += sum
-		fmt.Println(sum)
-	}
-	fmt.Println(sum)
-
-	// infinite
-	for {
-		fmt.Println("infinite!")
-	}
 }
