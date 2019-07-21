3fb934dfe46ed0beb223624599c6458f8fef3979
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-07-21 11:51:02 +0900

    Slice: make and cap

--- lesson.go
+++ lesson.go
@@ -5,30 +5,27 @@ import (
 )
 
 func main() {
-	var a [2]int
-	a[0] = 100
-	a[1] = 200
-	fmt.Println(a)
+	n := make([]int, 3, 5)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
 
-	var b []int = []int{100, 200}
-	b = append(b, 300)
-	fmt.Println(b)
+	n = append(n, 0, 0)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
 
-	n := []int{1, 2, 3, 4, 5, 6}
-	fmt.Println(n)
-	fmt.Println(n[2])
-	fmt.Println(n[2:4])
-	fmt.Println(n[2:])
-	fmt.Println(n[:2])
-	fmt.Println(n[:])
+	n = append(n, 1, 2, 3, 4)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
 
-	var board = [][]int{
-		[] int{0, 1, 2},
-		[] int{3, 4, 5},
-		[] int{6, 7, 8},
+	a := make([]int, 3)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(a), cap(a), a)
+
+	b := make([]int, 0)
+	var c []int
+	fmt.Printf("len=%d cap=%d value=%v\n", len(b), cap(b), b)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(c), cap(c), c)
+
+	c = make([]int, 0, 5)
+	for i := 0; i < 5; i++ {
+		c = append(c, i)
+		fmt.Println(c)
 	}
-	fmt.Println(board)
-
-	n = append(n, 100, 200, 300, 5239849)
-	fmt.Println(n)
+	fmt.Println(c)
 }
