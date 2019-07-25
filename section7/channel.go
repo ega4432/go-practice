dce59b32c5d7724f9fa413b30885b80f0566dae1
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-07-26 07:35:34 +0900

    Learn: channel

--- section7/channel.go
+++ section7/channel.go
@@ -2,8 +2,8 @@ package main
 
 import "fmt"
 
-// channel を使うと gorutine でデータのやりとりができる
-func goroutine(s []int, c chan int)  {
+// channel を使うと goroutine でデータのやりとりができる
+func goroutine1(s []int, c chan int)  {
 	sum := 0
 	for _, v := range s{
 		sum += v
@@ -11,9 +11,9 @@ func goroutine(s []int, c chan int)  {
 	c <- sum
 }
 
-func goroutine2(s []int, c chan int)  {
+func goroutine2(ss []int, c chan int)  {
 	sum := 0
-	for _, v := range s{
+	for _, v := range ss{
 		sum += v
 	}
 	c <- sum
@@ -21,11 +21,14 @@ func goroutine2(s []int, c chan int)  {
 
 func main()  {
 	s := []int{1, 2, 3, 4, 5}
+	ss := []int{2, 4, 6, 8, 10}
 	c := make(chan int)  // 15, 15
-	go goroutine(s, c)
-	go goroutine2(s, c)
+	go goroutine1(s, c)
+	go goroutine2(ss, c)
+	// goroutine1 で受け取ったチャネルを x に入れる
 	x := <-c
 	fmt.Println(x)
+	// goroutine2 で受け取ったチャネルを y に入れる
 	y := <-c
 	fmt.Println(y)
 }
