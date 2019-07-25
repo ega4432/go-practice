afba074a5d1c8b811236c43eef8a16832034fe4d
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-07-26 07:52:02 +0900

    Add: length

--- section7/channel_range_close.go
+++ section7/channel_range_close.go
@@ -14,7 +14,7 @@ func goroutine(s []int, c chan int)  {
 
 func main()  {
 	s := []int{1, 2, 3, 4, 5}
-	c := make(chan int)
+	c := make(chan int, len(s))
 	go goroutine(s, c)
 
 	// goroutine で受け取ったチャネルc次々に取り出していく
