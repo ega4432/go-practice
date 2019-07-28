58c75744ab8cf470a2da732004e21329871c501d
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-07-28 18:47:06 +0900

    Learn about sort package

--- section9/sort.go
+++ section9/sort.go
@@ -1,1 +1,34 @@
-package section9
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	i := []int{5, 3, 2, 8, 7}
+	s := []string{"d", "a", "f"}
+	p := []struct {
+		Name string
+		Age  int
+	}{
+		{"Mike", 20},
+		{"Vera", 40},
+		{"Nancy", 30},
+		{"Bob", 50},
+	}
+	fmt.Println(i, s, p)
+	sort.Ints(i)
+	fmt.Println(i)
+
+	sort.Strings(s)
+	fmt.Println(s)
+
+	//　p の Name 昇順
+	sort.Slice(p, func(i, j int) bool { return p[i].Name < p[j].Name })
+	fmt.Println(i, s, p)
+
+	// p の Age 昇順
+	sort.Slice(p, func(i, j int) bool { return p[i].Age < p[j].Age })
+	fmt.Println(i, s, p)
+}
