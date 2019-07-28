5a333510a87589c29f4ff6e87e62f6945b414822
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-07-28 16:04:23 +0900

    Learn about writting go document

--- section8/package/mylib/math.go
+++ section8/package/mylib/math.go
@@ -1,5 +1,21 @@
+/*
+mylib is yosemite's lib.
+*/
 package mylib
 
+import "fmt"
+
+// Person2 doc
+type Person2 struct {
+	Name string
+	Age  int
+}
+
+func (p *Person2) Say() {
+	fmt.Println("Person2")
+}
+
+// Average returns the average of a series fo numbers
 func Average(s []int) int {
 	total := 0
 	for _, i := range s {
