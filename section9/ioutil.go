ea635058af6d970a6077532fdcdfffd6f3de9f7d
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-07-28 19:34:38 +0900

    Learn about ioutil package

--- section9/ioutil.go
+++ section9/ioutil.go
@@ -1,1 +1,26 @@
-package section9
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	// "lesson.go" の中身を読み込む
+	content, err := ioutil.ReadFile("lesson.go")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(content))
+
+	// "ioutil_tmp.go" というファイルを作成して、書き込む
+	if err := ioutil.WriteFile("ioutil_tmp.go", content, 0666); err != nil {
+		log.Fatal(err)
+	}
+
+	r := bytes.NewBuffer([]byte("abc"))
+	content2, _ := ioutil.ReadAll(r)
+	fmt.Println(string(content2))
+}
