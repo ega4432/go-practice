85ed63589ea9657383ec5afd3feacbe713db18da
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-07-28 17:51:42 +0900

    Learn about time package

--- section9/time.go
+++ section9/time.go
@@ -1,1 +1,14 @@
-package section9
+// https://golang.org/pkg/time
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+	fmt.Println(t)
+	fmt.Println(t.Format(time.RFC3339))
+	fmt.Println(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
+}
