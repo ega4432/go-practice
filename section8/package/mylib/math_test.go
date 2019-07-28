3b78875669dbacff140b440d3fbfb282b6bc0584
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-07-28 15:24:37 +0900

    Learn about test of Go

---
+++ section8/package/mylib/math_test.go
@@ -0,0 +1,19 @@
+package mylib
+
+import (
+	"testing"
+)
+
+var Dubug bool = true
+
+func TestAverage(t *testing.T) {
+	if Dubug {
+		t.Skip("this is skip reason...")
+	}
+	v := Average([]int{1, 2, 3, 4, 5})
+	if v != 3 {
+		t.Error("Expected 3, got", v)
+	}
+	// テストの実行は "$ go test ./..." or Golan　からできる
+	// testing フレームワーク Ginkgo を使っても良い( https://onsi.github.io/ginkgo/ )
+}
