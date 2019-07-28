7f75e2274736a77a98ed98aea4bfff6830b7cdd7
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-07-28 19:22:18 +0900

    Learn about context package

--- section9/context.go
+++ section9/context.go
@@ -1,1 +1,40 @@
-package section9
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+// https://golang.org/pkg/context/
+func longProcess(ctx context.Context, ch chan string) {
+	fmt.Println("run")
+	time.Sleep(2 * time.Second)
+	fmt.Println("finish")
+	ch <- "result"
+}
+
+func main() {
+	ch := make(chan string)
+	ctx := context.Background()
+	// ctx := context.TODO()
+	// エラーとかにしたくない時はとりあえず TODO() にしておくこともある
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+	go longProcess(ctx, ch)
+	// goroutine がよばれたらすぐにキャンセルになる
+	// cancel()
+
+CTXLOOP:
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
+			break CTXLOOP
+		case <-ch:
+			fmt.Println("success")
+			break CTXLOOP
+		}
+	}
+	fmt.Println("#########################")
+}
