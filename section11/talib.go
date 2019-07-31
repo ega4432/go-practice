41f3d1a92c4a6f16bca58beac0c394cdf2a18899
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-07-31 21:55:59 +0900

    Learn about anlyzing stock prices with talib

--- section11/talib.go
+++ section11/talib.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/markcheno/go-quote"
+	"github.com/markcheno/go-talib"
+)
 
+func main() {
+	spy, _ := quote.NewQuoteFromYahoo(
+		"spy", "2018-04-01", "2019-01-01", quote.Daily, true)
+	fmt.Println(spy.CSV())
+	// Rsi = 株価が売られているか買われているかの指標、グラフ
+	rsi2 := talib.Rsi(spy.Close, 2)
+	fmt.Println(rsi2)
+	// moving average 過去14日間
+	mva := talib.Ema(spy.Close, 14)
+	fmt.Println(mva)
 }
