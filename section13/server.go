bb61c0e6683d7326e6349bfb31d9a53ec4d2c49a
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-08-01 23:47:25 +0900

    web server

---
+++ section13/server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// https://golang.org/doc/articles/wiki/
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	// /view/test
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func main() {
+	http.HandlerFunc("./view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
