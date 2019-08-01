0670f74ea273390c447f09e000597a9fdd3e2746
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-08-02 08:46:54 +0900

    Create template, endpointURL, handler

--- section13/server.go
+++ section13/server.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
 // https://golang.org/doc/articles/wiki/
@@ -27,16 +28,36 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	// /view/test
 	title := r.URL.Path[len("/view/"):]
 	title = "section13/" + title
 	log.Println(title)
 	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	// fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	// "section13" という文字列を置換して edit 用のリンクを使いたい
+	p.Title = strings.Replace(p.Title, "section13/", "", 1)
+	renderTemplate(w, "section13/view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	title = "section13/" + title
+	log.Println(title)
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "section13/edit", p)
 }
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
